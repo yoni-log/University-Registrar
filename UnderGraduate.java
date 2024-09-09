d95a23dbc66a3f5657f07c2e1b226207e0c69c96
ioanni <ioannisgiannopoulosloyola@gmail.com>
2024-09-09 10:42:21 -0500

    Update UnderGraduate.java

---
+++ UnderGraduate.java
@@ -0,0 +1,5 @@
+public class UnderGraduate extends Student {
+    public UnderGraduate(String ln, String fn, int iD, int day, int month, int year) {
+        super(ln, fn, iD, day, month, year , 0.13);
+    }
+}
